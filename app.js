766f2a5da62a03dcd5e9177aa54cf1dbd4e01375
igalmes <91505395+igalmes@users.noreply.github.com>
2022-07-27 19:38:30 -0300

    Add files via upload
    Proyecto LN

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname +'/public'));
+
+app.use(require('./router'));
+
+app.use((req, res, next) => {
+    res.status(404).send('Not found');
+});
+
+const port = process.env.PORT || 80;
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
