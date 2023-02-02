910e434c9c1e9589452fa34a81fb37c6f26628d4
ignaciogalmes <ignaciogalmes79@gmail.com>
2023-02-01 23:38:27 -0300

    CRUD

--- db.js
+++ db.js
@@ -5,16 +5,19 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    password: process.env.DD_PASS,
+    database: process.env.DB_NAME,
 });
 
 connection.connect(error => {
     if (error) { throw error }
 });
 
-connection.query('SELECT id, nombre FROM producto', (error, results) => {
-    if (error) { throw error }
+// connection.query('SELECT id, nombre FROM producto', (error, results) => {
+//     if (error) { throw error }
 
-    console.log(results);
-});
+//     console.log(results);
+// });
+
+//si alquien requiere mi modulo se hace un:
+module.exports = connection;
